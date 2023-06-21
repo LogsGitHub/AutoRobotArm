3685e88278848a577acebf62f66f9a87cc2540a2
LogsGitHub <136593813+LogsGitHub@users.noreply.github.com>
2023-06-21 21:00:43 +0800

    Add files via upload

--- AutoRobotArm/env.py
+++ AutoRobotArm/env.py
@@ -5,7 +5,7 @@ import pyglet
 class ArmEnv(object):
     viewer = None
     dt = .1    # refresh rate
-    action_bound = [-1, 1]
+    action_bound = [-1, 1] # 限制了角度
     goal = {'x': 100., 'y': 100., 'l': 40}
     state_dim = 9
     action_dim = 2
@@ -22,7 +22,10 @@ class ArmEnv(object):
         action = np.clip(action, *self.action_bound)
         self.arm_info['r'] += action * self.dt
         self.arm_info['r'] %= np.pi * 2    # normalize
-        self.arm_info['r'][1] = np.clip(self.arm_info['r'][1], 0, np.pi)
+        if self.arm_info['r'][1] > np.pi:  # 增加一个判断条件，如果前臂的旋转角度超过了180度
+            self.arm_info['r'][1] = np.pi  # 将前臂的旋转角度限制在180度以内
+        elif self.arm_info['r'][1] < -np.pi:  # 增加一个判断条件，如果前臂的旋转角度小于-180度
+            self.arm_info['r'][1] = -np.pi  # 将前臂的旋转角度限制在-180度以内
         (a1l, a2l) = self.arm_info['l']  # radius, arm length
         (a1r, a2r) = self.arm_info['r']  # radian, angle
         a1xy = np.array([200., 200.])    # a1 start (x0, y0)
@@ -31,8 +34,8 @@ class ArmEnv(object):
         # normalize features
         dist1 = [(self.goal['x'] - a1xy_[0]) / 400, (self.goal['y'] - a1xy_[1]) / 400]
         dist2 = [(self.goal['x'] - finger[0]) / 400, (self.goal['y'] - finger[1]) / 400]
-        r = -np.sqrt(dist2[0]**2+dist2[1]**2)
-
+        #r = -np.sqrt(dist2[0]**2+dist2[1]**2)
+        r = -np.sqrt(dist2[0] ** 2 + dist2[1] ** 2) - np.abs(self.arm_info['r'][1]) * 0.1
         # done and reward
         if self.goal['x'] - self.goal['l']/2 < finger[0] < self.goal['x'] + self.goal['l']/2:
             if self.goal['y'] - self.goal['l']/2 < finger[1] < self.goal['y'] + self.goal['l']/2:
@@ -40,6 +43,7 @@ class ArmEnv(object):
                 self.on_goal += 1
                 if self.on_goal > 50:
                     done = True
+                    action = [0., 0.]#增加一个终止条件，防止抖动
         else:
             self.on_goal = 0
 
@@ -157,3 +161,155 @@ if __name__ == '__main__':
     while True:
         env.render()
         env.step(env.sample_action())
+
+'''
+# 这是env.py部分：定义一个ArmEnv类，用来模拟一个双臂机器人的环境
+class ArmEnv(object):
+    viewer = None # 定义一个可视化对象，初始为空
+    dt = .1    # 刷新率，每0.1秒刷新一次
+    action_bound = [-1, 1] # 动作的范围，-1到1之间
+    goal = {'x': 100., 'y': 100., 'l': 40} # 目标的位置和大小，用一个字典表示
+    state_dim = 9 # 状态的维度，有9个特征
+    action_dim = 2 # 动作的维度，有2个动作
+
+    def __init__(self): # 初始化方法
+        self.arm_info = np.zeros( # 定义一个数组，用来存储两个臂的信息
+            2, dtype=[('l', np.float32), ('r', np.float32)])
+        self.arm_info['l'] = 100        # 两个臂的长度都是100
+        self.arm_info['r'] = np.pi/6    # 两个臂的初始角度都是pi/6
+        self.on_goal = 0 # 定义一个变量，用来记录机器人是否达到目标
+
+    def step(self, action): # 定义一个步进方法，用来更新状态和奖励
+        done = False # 定义一个变量，用来表示是否结束
+        action = np.clip(action, *self.action_bound) # 将动作限制在范围内
+        self.arm_info['r'] += action * self.dt # 根据动作和刷新率更新角度信息
+        self.arm_info['r'] %= np.pi * 2    # 将角度归一化到0到2pi之间
+
+        (a1l, a2l) = self.arm_info['l']  # 取出两个臂的长度
+        (a1r, a2r) = self.arm_info['r']  # 取出两个臂的角度
+        a1xy = np.array([200., 200.])    # 定义第一个臂的起点坐标（200，200）
+        a1xy_ = np.array([np.cos(a1r), np.sin(a1r)]) * a1l + a1xy  # 根据第一个臂的长度和角度计算第一个臂的终点坐标（也是第二个臂的起点坐标）
+        finger = np.array([np.cos(a1r + a2r), np.sin(a1r + a2r)]) * a2l + a1xy_  # 根据两个臂的长度和角度计算第二个臂的终点坐标（也是机器人的手指坐标）
+        # 归一化特征
+        dist1 = [(self.goal['x'] - a1xy_[0]) / 400, (self.goal['y'] - a1xy_[1]) / 400] # 计算第一个臂的终点到目标中心的距离，并除以400进行归一化
+        dist2 = [(self.goal['x'] - finger[0]) / 400, (self.goal['y'] - finger[1]) / 400] # 计算第二个臂的终点到目标中心的距离，并除以400进行归一化
+        r = -np.sqrt(dist2[0]**2+dist2[1]**2) # 根据第二个臂的终点到目标中心的距离计算奖励值，距离越小奖励越大
+
+        # 判断是否结束和奖励
+        if self.goal['x'] - self.goal['l']/2 < finger[0] < self.goal['x'] + self.goal['l']/2: # 如果第二个臂的终点的x坐标在目标的范围内
+            if self.goal['y'] - self.goal['l']/2 < finger[1] < self.goal['y'] + self.goal['l']/2: # 如果第二个臂的终点的y坐标在目标的范围内
+                r += 1. # 奖励值加1
+                self.on_goal += 1 # 记录达到目标的次数加1
+                if self.on_goal > 50: # 如果达到目标的次数超过50次
+                    done = True # 结束为真
+        else: # 否则
+            self.on_goal = 0 # 记录达到目标的次数归零
+
+        # 状态
+        s = np.concatenate((a1xy_/200, finger/200, dist1 + dist2, [1. if self.on_goal else 0.])) # 将所有特征拼接成一个数组，作为状态
+        return s, r, done # 返回状态，奖励和结束
+
+    def reset(self): # 定义一个重置方法，用来初始化环境
+        self.goal['x'] = np.random.rand()*400. # 随机生成目标的x坐标
+        self.goal['y'] = np.random.rand()*400. # 随机生成目标的y坐标
+        self.arm_info['r'] = 2 * np.pi * np.random.rand(2) # 随机生成两个臂的角度
+        self.on_goal = 0 # 记录达到目标的次数归零
+        (a1l, a2l) = self.arm_info['l']  # 取出两个臂的长度
+        (a1r, a2r) = self.arm_info['r']  # 取出两个臂的角度
+        a1xy = np.array([200., 200.])  # 定义第一个臂的起点坐标（200，200）
+        a1xy_ = np.array([np.cos(a1r), np.sin(a1r)]) * a1l + a1xy  # 根据第一个臂的长度和角度计算第一个臂的终点坐标（也是第二个臂的起点坐标）
+        finger = np.array([np.cos(a1r + a2r), np.sin(a1r + a2r)]) * a2l + a1xy_  # 根据两个臂的长度和角度计算第二个臂的终点坐标（也是机器人的手指坐标）
+        # 归一化特征
+        dist1 = [(self.goal['x'] - a1xy_[0])/400, (self.goal['y'] - a1xy_[1])/400] # 计算第一个臂的终点到目标中心的距离，并除以400进行归一化
+        dist2 = [(self.goal['x'] - finger[0])/400, (self.goal['y'] - finger[1])/400] # 计算第二个臂的终点到目标中心的距离，并除以400进行归一化
+        # 状态
+        s = np.concatenate((a1xy_/200, finger/200, dist1 + dist2, [1. if self.on_goal else 0.])) # 将所有特征拼接成一个数组，作为状态
+        return s # 返回状态
+
+    def render(self): # 定义一个渲染方法，用来显示环境
+        if self.viewer is None: # 如果可视化对象为空
+            self.viewer = Viewer(self.arm_info, self.goal) # 创建一个Viewer对象，传入两个臂的信息和目标信息
+        self.viewer.render() # 调用Viewer对象的渲染方法
+
+    def sample_action(self): # 定义一个采样动作方法，用来随机生成动作
+        return np.random.rand(2)-0.5 # 返回一个-0.5到0.5之间的随机数组，作为'''
+'''
+# 定义一个Viewer类，继承自pyglet.window.Window类，用来显示双臂机器人的环境
+class Viewer(pyglet.window.Window):
+    bar_thc = 5 # 定义一个常量，表示臂的厚度
+
+    def __init__(self, arm_info, goal): # 初始化方法，传入两个臂的信息和目标信息
+        # vsync=False to not use the monitor FPS, we can speed up training
+        super(Viewer, self).__init__(width=400, height=400, resizable=False, caption='Arm', vsync=False) # 调用父类的初始化方法，设置窗口的宽度，高度，是否可调整大小，标题和垂直同步
+        pyglet.gl.glClearColor(1, 1, 1, 1) # 设置窗口的背景颜色为白色
+        self.arm_info = arm_info # 将两个臂的信息赋值给实例属性
+        self.goal_info = goal # 将目标信息赋值给实例属性
+        self.center_coord = np.array([200, 200]) # 定义一个数组，表示窗口的中心坐标（200，200）
+
+        self.batch = pyglet.graphics.Batch()    # display whole batch at once
+        self.goal = self.batch.add( # 在批处理中添加一个目标对象，用四边形表示
+            4, pyglet.gl.GL_QUADS, None,    # 4 corners
+            ('v2f', [goal['x'] - goal['l'] / 2, goal['y'] - goal['l'] / 2,                # location
+                     goal['x'] - goal['l'] / 2, goal['y'] + goal['l'] / 2,
+                     goal['x'] + goal['l'] / 2, goal['y'] + goal['l'] / 2,
+                     goal['x'] + goal['l'] / 2, goal['y'] - goal['l'] / 2]),
+            ('c3B', (86, 109, 249) * 4))    # color
+        self.arm1 = self.batch.add( # 在批处理中添加一个第一个臂对象，用四边形表示
+            4, pyglet.gl.GL_QUADS, None,
+            ('v2f', [250, 250,                # location
+                     250, 300,
+                     260, 300,
+                     260, 250]),
+            ('c3B', (249, 86, 86) * 4,))    # color
+        self.arm2 = self.batch.add( # 在批处理中添加一个第二个臂对象，用四边形表示
+            4, pyglet.gl.GL_QUADS, None,
+            ('v2f', [100, 150,              # location
+                     100, 160,
+                     200, 160,
+                     200, 150]), ('c3B', (249, 86, 86) * 4,))
+    
+    def render(self): # 定义一个渲染方法，用来显示环境
+        self._update_arm() # 调用私有方法更新臂的位置和角度
+        self.switch_to() # 切换到当前窗口
+        self.dispatch_events() # 处理窗口事件
+        self.dispatch_event('on_draw') # 触发绘制事件
+        self.flip() # 刷新窗口
+
+    def on_draw(self): # 定义一个绘制方法，用来绘制批处理中的对象
+        self.clear() # 清空窗口内容
+        self.batch.draw() # 绘制批处理中的对象
+
+    def _update_arm(self): # 定义一个私有方法，用来更新臂的位置和角度
+        # update goal
+        self.goal.vertices = ( # 根据目标信息更新目标对象的顶点坐标
+            self.goal_info['x'] - self.goal_info['l']/2, self.goal_info['y'] - self.goal_info['l']/2,
+            self.goal_info['x'] + self.goal_info['l']/2, self.goal_info['y'] - self.goal_info['l']/2,
+            self.goal_info['x'] + self.goal_info['l']/2, self.goal_info['y'] + self.goal_info['l']/2,
+            self.goal_info['x'] - self.goal_info['l']/2, self.goal_info['y'] + self.goal_info['l']/2)
+
+        # update arm
+        (a1l, a2l) = self.arm_info['l']     # 取出两个臂的长度
+        (a1r, a2r) = self.arm_info['r']     # 取出两个臂的角度
+        a1xy = self.center_coord            # 取出第一个臂的起点坐标（200，200）
+        a1xy_ = np.array([np.cos(a1r), np.sin(a1r)]) * a1l + a1xy   # 根据第一个臂的长度和角度计算第一个臂的终点坐标（也是第二个臂的起点坐标）
+        a2xy_ = np.array([np.cos(a1r+a2r), np.sin(a1r+a2r)]) * a2l + a1xy_  # 根据两个臂的长度和角度计算第二个臂的终点坐标（也是机器人的手指坐标）
+
+        a1tr, a2tr = np.pi / 2 - self.arm_info['r'][0], np.pi / 2 - self.arm_info['r'].sum() # 计算两个臂的垂直角度
+        xy01 = a1xy + np.array([-np.cos(a1tr), np.sin(a1tr)]) * self.bar_thc # 计算第一个臂对象的左上角顶点坐标
+        xy02 = a1xy + np.array([np.cos(a1tr), -np.sin(a1tr)]) * self.bar_thc # 计算第一个臂对象的左下角顶点坐标
+        xy11 = a1xy_ + np.array([np.cos(a1tr), -np.sin(a1tr)]) * self.bar_thc # 计算第一个臂对象的右下角顶点坐标
+        xy12 = a1xy_ + np.array([-np.cos(a1tr), np.sin(a1tr)]) * self.bar_thc # 计算第一个臂对象的右上角顶点坐标
+
+        xy11_ = a1xy_ + np.array([np.cos(a2tr), -np.sin(a2tr)]) * self.bar_thc # 计算第二个臂对象的左上角顶点坐标
+        xy12_ = a1xy_ + np.array([-np.cos(a2tr), np.sin(a2tr)]) * self.bar_thc # 计算第二个臂对象的左下角顶点坐标
+        xy21 = a2xy_ + np.array([-np.cos(a2tr), np.sin(a2tr)]) * self.bar_thc # 计算第二个臂对象的右上角顶点坐标
+        xy22 = a2xy_ + np.array([np.cos(a2tr), -np.sin(a2tr)]) * self.bar_thc # 计算第二个臂对象的右下角顶点坐标
+
+        self.arm1.vertices = np.concatenate((xy01, xy02, xy11, xy12)) # 根据计算出来的顶点坐标更新第一个臂对象的顶点属性
+        self.arm2.vertices = np.concatenate((xy11_, xy12_, xy21, xy22)) # 根据计算出来的顶点坐标更新第二个臂对象的顶点属性
+
+    # convert the mouse coordinate to goal's coordinate
+    def on_mouse_motion(self, x, y, dx, dy): # 定义一个鼠标移动事件处理方法，用来更新目标信息
+        self.goal_info['x'] = x # 将鼠标的x坐标赋值给目标信息中的x属性
+        self.goal_info['y'] = y #
+'''
--- AutoRobotArm/rl.py
+++ AutoRobotArm/rl.py
@@ -1,4 +1,6 @@
-import tensorflow as tf
+import torch
+import torch.nn as nn
+import torch.optim as optim
 import numpy as np
 
 #####################  hyper parameters  ####################
@@ -11,92 +13,210 @@ MEMORY_CAPACITY = 30000
 BATCH_SIZE = 32
 
 
+class Actor(nn.Module):
+    def __init__(self, s_dim, a_dim, a_bound):
+        super(Actor, self).__init__()
+        self.fc1 = nn.Linear(s_dim, 300)
+        self.fc2 = nn.Linear(300, a_dim)
+        self.a_bound = a_bound
+
+    def forward(self, x):
+        x = torch.relu(self.fc1(x))
+        x = torch.tanh(self.fc2(x))
+        return x * self.a_bound
+
+
+class Critic(nn.Module):
+    def __init__(self, s_dim, a_dim):
+        super(Critic, self).__init__()
+        self.fc1 = nn.Linear(s_dim + a_dim, 300)
+        self.fc2 = nn.Linear(300, 1)
+
+    def forward(self, s, a):
+        x = torch.cat([s, a], 1)
+        x = torch.relu(self.fc1(x))
+        return self.fc2(x)
+
+
 class DDPG(object):
     def __init__(self, a_dim, s_dim, a_bound,):
         self.memory = np.zeros((MEMORY_CAPACITY, s_dim * 2 + a_dim + 1), dtype=np.float32)
         self.pointer = 0
         self.memory_full = False
-        self.sess = tf.Session()
-        self.a_replace_counter, self.c_replace_counter = 0, 0
 
         self.a_dim, self.s_dim, self.a_bound = a_dim, s_dim, a_bound[1]
-        self.S = tf.placeholder(tf.float32, [None, s_dim], 's')
-        self.S_ = tf.placeholder(tf.float32, [None, s_dim], 's_')
-        self.R = tf.placeholder(tf.float32, [None, 1], 'r')
+        self.actor_eval = Actor(s_dim, a_dim, self.a_bound)
+        self.actor_target = Actor(s_dim, a_dim, self.a_bound)
+        self.critic_eval = Critic(s_dim, a_dim)
+        self.critic_target = Critic(s_dim, a_dim)
 
-        with tf.variable_scope('Actor'):
-            self.a = self._build_a(self.S, scope='eval', trainable=True)
-            a_ = self._build_a(self.S_, scope='target', trainable=False)
-        with tf.variable_scope('Critic'):
-            # assign self.a = a in memory when calculating q for td_error,
-            # otherwise the self.a is from Actor when updating Actor
-            q = self._build_c(self.S, self.a, scope='eval', trainable=True)
-            q_ = self._build_c(self.S_, a_, scope='target', trainable=False)
-
-        # networks parameters
-        self.ae_params = tf.get_collection(tf.GraphKeys.GLOBAL_VARIABLES, scope='Actor/eval')
-        self.at_params = tf.get_collection(tf.GraphKeys.GLOBAL_VARIABLES, scope='Actor/target')
-        self.ce_params = tf.get_collection(tf.GraphKeys.GLOBAL_VARIABLES, scope='Critic/eval')
-        self.ct_params = tf.get_collection(tf.GraphKeys.GLOBAL_VARIABLES, scope='Critic/target')
-
-        # target net replacement
-        self.soft_replace = [[tf.assign(ta, (1 - TAU) * ta + TAU * ea), tf.assign(tc, (1 - TAU) * tc + TAU * ec)]
-                             for ta, ea, tc, ec in zip(self.at_params, self.ae_params, self.ct_params, self.ce_params)]
-
-        q_target = self.R + GAMMA * q_
-        # in the feed_dic for the td_error, the self.a should change to actions in memory
-        td_error = tf.losses.mean_squared_error(labels=q_target, predictions=q)
-        self.ctrain = tf.train.AdamOptimizer(LR_C).minimize(td_error, var_list=self.ce_params)
-
-        a_loss = - tf.reduce_mean(q)    # maximize the q
-        self.atrain = tf.train.AdamOptimizer(LR_A).minimize(a_loss, var_list=self.ae_params)
-
-        self.sess.run(tf.global_variables_initializer())
+        self.optimizer_actor = optim.Adam(self.actor_eval.parameters(), lr=LR_A)
+        self.optimizer_critic = optim.Adam(self.critic_eval.parameters(), lr=LR_C)
+        self.loss_func = nn.MSELoss()
 
     def choose_action(self, s):
-        return self.sess.run(self.a, {self.S: s[None, :]})[0]
+        s = torch.FloatTensor(s).unsqueeze(0)
+        return self.actor_eval(s).detach().numpy()[0]
 
     def learn(self):
         # soft target replacement
-        self.sess.run(self.soft_replace)
+        for target_param, eval_param in zip(self.actor_target.parameters(), self.actor_eval.parameters()):
+            target_param.data.copy_(TAU * eval_param.data + (1 - TAU) * target_param.data)
+
+        for target_param, eval_param in zip(self.critic_target.parameters(), self.critic_eval.parameters()):
+            target_param.data.copy_(TAU * eval_param.data + (1 - TAU) * target_param.data)
 
         indices = np.random.choice(MEMORY_CAPACITY, size=BATCH_SIZE)
         bt = self.memory[indices, :]
-        bs = bt[:, :self.s_dim]
-        ba = bt[:, self.s_dim: self.s_dim + self.a_dim]
-        br = bt[:, -self.s_dim - 1: -self.s_dim]
-        bs_ = bt[:, -self.s_dim:]
+        bs = torch.FloatTensor(bt[:, :self.s_dim])
+        ba = torch.FloatTensor(bt[:, self.s_dim: self.s_dim + self.a_dim])
+        br = torch.FloatTensor(bt[:, -self.s_dim - 1: -self.s_dim])
+        bs_ = torch.FloatTensor(bt[:, -self.s_dim:])
 
-        self.sess.run(self.atrain, {self.S: bs})
-        self.sess.run(self.ctrain, {self.S: bs, self.a: ba, self.R: br, self.S_: bs_})
+        # Critic update
+        a_ = self.actor_target(bs_)
+        q_ = self.critic_target(bs_, a_)
+        q_target = br + GAMMA * q_
+        q_eval = self.critic_eval(bs, ba)
+        critic_loss = self.loss_func(q_eval, q_target)
+        self.optimizer_critic.zero_grad()
+        critic_loss.backward()
+        torch.nn.utils.clip_grad_norm_(self.critic_eval.parameters(), 10)  # 增加一个梯度裁剪的操作，限制critic评估网络的梯度范数在10以内
+        self.optimizer_critic.step()
+
+        # Actor update
+        a = self.actor_eval(bs)
+        q = self.critic_eval(bs, a)
+        actor_loss = -torch.mean(q)
+        self.optimizer_actor.zero_grad()
+        actor_loss.backward()
+        torch.nn.utils.clip_grad_norm_(self.actor_eval.parameters(), 10)  # 增加一个梯度裁剪的操作，限制actor评估网络的梯度范数在10以内
+        self.optimizer_actor.step()
 
     def store_transition(self, s, a, r, s_):
         transition = np.hstack((s, a, [r], s_))
+        r -= np.sum(np.square(a)) * 0.1  # 增加一个动作惩罚项，使得动作越小越好
         index = self.pointer % MEMORY_CAPACITY  # replace the old memory with new memory
         self.memory[index, :] = transition
         self.pointer += 1
         if self.pointer > MEMORY_CAPACITY:      # indicator for learning
             self.memory_full = True
 
-    def _build_a(self, s, scope, trainable):
-        with tf.variable_scope(scope):
-            net = tf.layers.dense(s, 300, activation=tf.nn.relu, name='l1', trainable=trainable)
-            a = tf.layers.dense(net, self.a_dim, activation=tf.nn.tanh, name='a', trainable=trainable)
-            return tf.multiply(a, self.a_bound, name='scaled_a')
+    def save(self, path='./params'):
+        torch.save(self.actor_eval.state_dict(), path + '_actor.pth')
+        torch.save(self.critic_eval.state_dict(), path + '_critic.pth')
 
-    def _build_c(self, s, a, scope, trainable):
-        with tf.variable_scope(scope):
-            n_l1 = 300
-            w1_s = tf.get_variable('w1_s', [self.s_dim, n_l1], trainable=trainable)
-            w1_a = tf.get_variable('w1_a', [self.a_dim, n_l1], trainable=trainable)
-            b1 = tf.get_variable('b1', [1, n_l1], trainable=trainable)
-            net = tf.nn.relu(tf.matmul(s, w1_s) + tf.matmul(a, w1_a) + b1)
-            return tf.layers.dense(net, 1, trainable=trainable)  # Q(s,a)
+    def restore(self, path='./params'):
+        self.actor_eval.load_state_dict(torch.load(path + '_actor.pth'))
+        self.critic_eval.load_state_dict(torch.load(path + '_critic.pth'))
+'''
+# 导入torch，torch.nn，torch.optim和numpy模块
+import torch
+import torch.nn as nn
+import torch.optim as optim
+import numpy as np
 
-    def save(self):
-        saver = tf.train.Saver()
-        saver.save(self.sess, './params', write_meta_graph=False)
+#####################  hyper parameters  ####################
 
-    def restore(self):
-        saver = tf.train.Saver()
-        saver.restore(self.sess, './params')
+LR_A = 0.001    # 定义一个常量，表示actor的学习率
+LR_C = 0.001    # 定义一个常量，表示critic的学习率
+GAMMA = 0.9     # 定义一个常量，表示奖励的折扣因子
+TAU = 0.01      # 定义一个常量，表示软更新的系数
+MEMORY_CAPACITY = 30000 # 定义一个常量，表示记忆库的容量
+BATCH_SIZE = 32 # 定义一个常量，表示每次训练的批次大小
+
+
+class Actor(nn.Module): # 定义一个Actor类，继承自nn.Module类，用来构建actor网络
+    def __init__(self, s_dim, a_dim, a_bound): # 初始化方法，传入状态维度，动作维度和动作范围
+        super(Actor, self).__init__() # 调用父类的初始化方法
+        self.fc1 = nn.Linear(s_dim, 300) # 定义一个全连接层，输入为状态维度，输出为300
+        self.fc2 = nn.Linear(300, a_dim) # 定义一个全连接层，输入为300，输出为动作维度
+        self.a_bound = a_bound # 将动作范围赋值给实例属性
+
+    def forward(self, x): # 前向传播方法，传入状态x
+        x = torch.relu(self.fc1(x)) # 将x通过第一个全连接层，并使用relu激活函数
+        x = torch.tanh(self.fc2(x)) # 将x通过第二个全连接层，并使用tanh激活函数
+        return x * self.a_bound # 返回x乘以动作范围，作为输出动作
+
+
+class Critic(nn.Module): # 定义一个Critic类，继承自nn.Module类，用来构建critic网络
+    def __init__(self, s_dim, a_dim): # 初始化方法，传入状态维度和动作维度
+        super(Critic, self).__init__() # 调用父类的初始化方法
+        self.fc1 = nn.Linear(s_dim + a_dim, 300) # 定义一个全连接层，输入为状态维度加动作维度，输出为300
+        self.fc2 = nn.Linear(300, 1) # 定义一个全连接层，输入为300，输出为1
+
+    def forward(self, s, a): # 前向传播方法，传入状态s和动作a
+        x = torch.cat([s, a], 1) # 将s和a拼接在一起，作为输入x
+        x = torch.relu(self.fc1(x)) # 将x通过第一个全连接层，并使用relu激活函数
+        return self.fc2(x) # 将x通过第二个全连接层，并返回输出值
+
+
+class DDPG(object): # 定义一个DDPG类，用来实现DDPG算法
+    def __init__(self, a_dim, s_dim, a_bound,): # 初始化方法，传入动作维度，状态维度和动作范围
+        self.memory = np.zeros((MEMORY_CAPACITY, s_dim * 2 + a_dim + 1), dtype=np.float32) # 定义一个数组，用来存储记忆库中的转移元组（s,a,r,s_）
+        self.pointer = 0 # 定义一个指针，用来记录记忆库中存储的数量
+        self.memory_full = False # 定义一个布尔值，用来表示记忆库是否已满
+
+        self.a_dim, self.s_dim, self.a_bound = a_dim, s_dim, a_bound[1] # 将动作维度，状态维度和动作范围赋值给实例属性
+        self.actor_eval = Actor(s_dim, a_dim, self.a_bound) # 创建一个Actor对象，作为评估网络
+        self.actor_target = Actor(s_dim, a_dim, self.a_bound) # 创建一个Actor对象，作为目标网络
+        self.critic_eval = Critic(s_dim, a_dim) # 创建一个Critic对象，作为评估网络
+        self.critic_target = Critic(s_dim, a_dim) # 创建一个Critic对象，作为目标网络
+
+        self.optimizer_actor = optim.Adam(self.actor_eval.parameters(), lr=LR_A) # 定义一个优化器，用来更新actor评估网络的参数
+        self.optimizer_critic = optim.Adam(self.critic_eval.parameters(), lr=LR_C) # 定义一个优化器，用来更新critic评估网络的参数
+        self.loss_func = nn.MSELoss() # 定义一个损失函数，用来计算critic评估网络的均方误差
+
+    def choose_action(self, s): # 定义一个选择动作的方法，传入状态s
+        s = torch.FloatTensor(s).unsqueeze(0) # 将s转换为torch张量，并增加一个维度
+        return self.actor_eval(s).detach().numpy()[0] # 将s输入actor评估网络，得到动作，并转换为numpy数组，并去掉多余的维度
+
+    def learn(self): # 定义一个学习的方法，用来更新网络参数
+        # soft target replacement
+        for target_param, eval_param in zip(self.actor_target.parameters(), self.actor_eval.parameters()): # 遍历actor目标网络和评估网络的参数
+            target_param.data.copy_(TAU * eval_param.data + (1 - TAU) * target_param.data) # 用软更新的方式更新目标网络的参数
+
+        for target_param, eval_param in zip(self.critic_target.parameters(), self.critic_eval.parameters()): # 遍历critic目标网络和评估网络的参数
+            target_param.data.copy_(TAU * eval_param.data + (1 - TAU) * target_param.data) # 用软更新的方式更新目标网络的参数
+
+        indices = np.random.choice(MEMORY_CAPACITY, size=BATCH_SIZE) # 从记忆库中随机采样一批索引
+        bt = self.memory[indices, :] # 根据索引取出一批转移元组
+        bs = torch.FloatTensor(bt[:, :self.s_dim]) # 取出一批状态，并转换为torch张量
+        ba = torch.FloatTensor(bt[:, self.s_dim: self.s_dim + self.a_dim]) # 取出一批动作，并转换为torch张量
+        br = torch.FloatTensor(bt[:, -self.s_dim - 1: -self.s_dim]) # 取出一批奖励，并转换为torch张量
+        bs_ = torch.FloatTensor(bt[:, -self.s_dim:]) # 取出一批下一状态，并转换为torch张量
+
+        # Critic update
+        a_ = self.actor_target(bs_) # 将下一状态输入actor目标网络，得到下一动作
+        q_ = self.critic_target(bs_, a_) # 将下一状态和下一动作输入critic目标网络，得到下一价值
+        q_target = br + GAMMA * q_ # 根据奖励和折扣因子计算目标价值
+        q_eval = self.critic_eval(bs, ba) # 将状态和动作输入critic评估网络，得到评估价值
+        critic_loss = self.loss_func(q_eval, q_target) # 根据评估价值和目标价值计算critic评估网络的损失函数
+        self.optimizer_critic.zero_grad() # 清空优化器中的梯度
+        critic_loss.backward() # 反向传播计算梯度
+        self.optimizer_critic.step() # 更新critic评估网络的参数
+
+               # Actor update
+        a = self.actor_eval(bs) # 将状态输入actor评估网络，得到动作
+        q = self.critic_eval(bs, a) # 将状态和动作输入critic评估网络，得到价值
+        actor_loss = -torch.mean(q) # 根据价值计算actor评估网络的损失函数，取负号是为了梯度上升
+        self.optimizer_actor.zero_grad() # 清空优化器中的梯度
+        actor_loss.backward() # 反向传播计算梯度
+        self.optimizer_actor.step() # 更新actor评估网络的参数
+
+    def store_transition(self, s, a, r, s_): # 定义一个存储转移元组的方法，传入状态，动作，奖励和下一状态
+        transition = np.hstack((s, a, [r], s_)) # 将转移元组拼接在一起，作为一个数组
+        index = self.pointer % MEMORY_CAPACITY  # replace the old memory with new memory
+        self.memory[index, :] = transition # 将转移元组存储到记忆库中，根据指针位置替换旧的记忆
+        self.pointer += 1 # 指针加一
+        if self.pointer > MEMORY_CAPACITY:      # indicator for learning
+            self.memory_full = True # 如果指针超过记忆库容量，表示记忆库已满
+
+    def save(self, path='./params'): # 定义一个保存网络参数的方法，传入保存路径，默认为'./params'
+        torch.save(self.actor_eval.state_dict(), path + '_actor.pth') # 保存actor评估网络的参数到指定路径
+        torch.save(self.critic_eval.state_dict(), path + '_critic.pth') # 保存critic评估网络的参数到指定路径
+
+    def restore(self, path='./params'): # 定义一个恢复网络参数的方法，传入恢复路径，默认为'./params'
+        self.actor_eval.load_state_dict(torch.load(path + '_actor.pth')) # 加载actor评估网络的参数从指定路径
+        self.critic_eval.load_state_dict(torch.load(path + '_critic.pth')) # 加载critic评估网络的参数从指定路径
+'''
