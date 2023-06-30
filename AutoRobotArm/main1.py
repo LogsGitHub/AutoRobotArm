4fc79e7ce0a28b335808c375c4cc5a45a313bb1a
LogsGitHub <136593813+LogsGitHub@users.noreply.github.com>
2023-06-30 14:16:51 +0800

    Update main1.py

--- AutoRobotArm/main1.py
+++ AutoRobotArm/main1.py
@@ -1,11 +1,20 @@
 # 导入env和rl模块，分别定义了环境类和算法类
-from env import ArmEnv
+from env1 import ArmEnv
 from rlp import DDPG
+import numpy as np
+import seaborn as sns  # 导入seaborn库
+import matplotlib.pyplot as plt  # 导入matplotlib库
+import time
 
-MAX_EPISODES = 1200  # 定义一个常量，表示最大训练回合数
-MAX_EP_STEPS = 200  # 定义一个常量，表示每个回合的最大步数
-ON_TRAIN = False  # 定义一个布尔值，表示是否进行训练
+rewards = []  # 定义一个空列表，用来存储每个回合的累积奖励
 
+MAX_EPISODES = 2000  # 定义一个常量，表示最大训练回合数
+MAX_EP_STEPS = 300  # 定义一个常量，表示每个回合的最大步数
+ON_TRAIN = True  # 定义一个布尔值，表示是否进行训练
+import seaborn as sns  # 导入seaborn库
+import matplotlib.pyplot as plt  # 导入matplotlib库
+
+rewards = []  # 定义一个空列表，用来存储每个回合的累积奖励
 # set env
 env = ArmEnv()  # 创建一个ArmEnv对象，作为环境
 s_dim = env.state_dim  # 取出环境的状态维度
@@ -17,14 +26,24 @@ rl = DDPG(a_dim, s_dim, a_bound)  # 创建一个DDPG对象，作为算法
 
 steps = []  # 定义一个空列表，用来存储步数
 
+n_training_steps = MAX_EPISODES * MAX_EP_STEPS
+n_steps_to_reduce_sigma = n_training_steps // 10
+initial_sigma = 0.2
+final_sigma = 0.01
+
+
+# ...
 
 def train():  # 定义一个训练的函数
+    global_step = 0  # 添加一个全局步数计数器
+
     # start training
     for i in range(MAX_EPISODES):  # 遍历每个回合
         s = env.reset()  # 重置环境，得到初始状态
         ep_r = 0.  # 定义一个变量，用来记录累积奖励
+
         for j in range(MAX_EP_STEPS):  # 遍历每个步骤
-            #env.render()
+            # env.render()
 
             a = rl.choose_action(s)  # 根据状态选择动作
 
@@ -38,13 +57,38 @@ def train():  # 定义一个训练的函数
                 rl.learn()  # 开始学习更新网络参数
 
             s = s_  # 将下一状态赋值给当前状态
+
+            # 根据需要逐步减小噪声
+            if global_step % n_steps_to_reduce_sigma == 0:
+                current_sigma = initial_sigma - (global_step / n_training_steps) * (initial_sigma - final_sigma)
+                rl.noise.set_sigma(current_sigma)
+
+            global_step += 1  # 更新全局步数计数器
+
             if done or j == MAX_EP_STEPS - 1:  # 如果结束或达到最大步数
+                if done: 
+                    ep_r = 50
+               
                 print('Ep: %i | %s | ep_r: %.1f | step: %i' % (
-                i, '---' if not done else 'done', ep_r, j))  # 打印回合数，结束标志，累积奖励和步数
+                    i, '---' if not done else 'done', ep_r, j))  # 打印回合数，结束标志，累积奖励和步数
                 break  # 跳出循环
-    rl.save()  # 保存网络参数
+        rewards.append(ep_r)  # 将每个回合的累积奖励添加到列表中
+    end_time = time.time()
+    elapsed_time = end_time - start_time
+    print(f"代码运行时间：{elapsed_time:.2f}秒")
+    rl.save()
+    episodes = range(1, MAX_EPISODES + 1)  # 定义一个列表，表示回合数
+    sns.relplot(x=episodes, y=rewards, kind="line", errorbar="sd")
+    plt.xlabel("Episode")  # 设置x轴标签为Episode
+    plt.ylabel("Accumulated Reward")  # 设置y轴标签为Accumulated Reward
+    plt.title("Learning Curve")  # 设置标题为Learning Curve
+    plt.ylim(-400, 400)
+    xticks = np.arange(0, MAX_EPISODES + 1, 100)  # 设置从0开始，间隔为100的刻度位置
+    plt.xticks(xticks)  #
+    plt.show()  # 显示图像
 
-
+def moving_average(data, window_size):
+    return np.convolve(data, np.ones(window_size)/window_size, mode='valid')
 def eval():  # 定义一个评估的函数
     rl.restore()  # 恢复网络参数
     env.render()  # 显示环境
@@ -57,6 +101,7 @@ def eval():  # 定义一个评估的函数
 
 
 if ON_TRAIN:  # 如果进行训练
+    start_time = time.time()
     train()  # 调用训练函数
 
 else:  # 否则
